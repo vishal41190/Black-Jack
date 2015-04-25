d3a36d4ea9113afbc0ee64bc425d972a3cc2f746
vishal <vishal41190@gmail.com>
2015-04-24 21:55:09 -0700

    Started implementing with socket.io and angular-js

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var socketIo = require("socket.io");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var game = require('./routes/game');
+var game = require('./model/game');
 var test = require('./routes/test');
 var databaseUrl = "mongodb://localhost:27017/blackjack";
 var app = express();
 var server = http.createServer(app);
-var io = socketIo(server);
+io = socketIo(server);
 server.listen(3000);
 
 // view engine setup
@@ -31,13 +31,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/game',game);
+
 app.use('/test',test);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -45,23 +45,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
@@ -70,3 +70,106 @@ module.exports = app;
 
 //Other resource
 // Card image from http://www.jfitz.com/cards/classic-playing-cards.png
+
+
+
+//Socket IO programming
+
+function getPlayer(players,plId){
+   
+    var temp = players.filter(function(items){
+       return items.playerId==plId;
+    });
+    return temp[0];
+}
+function getOtherPlayer(players,plId){
+    var temp = players.filter(function(items){
+        items.playerId!= plId;
+    });
+    return temp;
+}
+
+function removeSocketData(plyr){
+    delete plyr.socket;
+}
+
+function getLiveTable(playerId,tableIndex){
+
+    var temp = {};
+    
+    var tbl = JSON.parse(JSON.stringify(tables[tableIndex]));
+  //  console.log(tbl.players);
+    temp.tableName=tbl.tableName;
+    temp.player = getPlayer(tbl.players,playerId);
+    temp.otherPlayer = getOtherPlayer(tbl.players,playerId);
+    temp.dealer = tbl.dealer;
+    delete temp.player.socket;
+    for(var i =0; i<temp.otherPlayer.length;i++){
+       delete temp.otherPlayer[i].socket;
+    }
+    //console.log(temp);
+    return temp;
+
+}
+
+function findPlayer(playerId){
+    
+    for(var i =0; i<tables.length ;i++){
+       var elementPos = tables[i].players.map(function(x) {return x.playerId; }).indexOf(playerId);
+        if(elementPos>=0){
+        return elementPos;
+        }
+    }
+}
+
+
+var tables=[];
+var table = {
+
+    tableName: "",
+    players:[],
+    numberOfPlayer: 0,
+    dealer : null,
+    stack: [],
+    stackIndex:0
+
+
+};
+var liveTable={
+    tableName:"afsf",
+    otherPlayer:[],
+    player:{ cards:[], status:"", total:3},
+    dealer:{opencards:[],blindedCard:"",total:10}
+};
+io.on("connection",function(sct){
+    console.log("connected");
+
+    sct.on("AddMe",function(data){
+        
+        table= game.findTableForMe(JSON.stringify(tables));
+        var player = JSON.parse(JSON.stringify(data));
+        player.cards =[];
+        player.status="waiting";
+        player.total =0;
+        player.socket=sct;
+       game.addMeToTable(player,table);
+        
+        var liveTable=getLiveTable(player.playerId,findPlayer(player.playerId));
+        console.log(tables[0].players[0]);
+        tables[0].players[0].socket.emit("added",liveTable);
+        //io.socket(sct.id).emit("added",liveTable);
+        //sct.emit("added",liveTable);
+       
+        
+        
+        
+        
+        
+    });
+sct.on("error",function(err){
+   console.log(err); 
+});
+
+});
+
+
--- model/game.js
+++ model/game.js
@@ -1,32 +1,65 @@
 
-exports.table=function(){
-    this.tableId;
-    this.players=[];
-    this.findTableForMe = findTableForMe;
-    this.newGame = newGame;
-    this.hit = hit;
-    this.stand= stand;
-    this.deal =deal;
+var stackModel = require('../model/stack');
+
+
+var tables=[];
+
+//Connect to database and get collection
+
+
+function generateTableName() {
+    var temp = Date.now();
+    return (temp.toString(36));
+
 }
 
-newTable = function(){
-    this.tableId = "abc";
-}
-findTableForMe = function(player){
+
+
+
+
+
+function createNewTable(){
+  
+    var stack = new stackModel.Stack();
+    stack.makeDeck(2);
+    stack.shuffle(5);
+    var table = {
+        tableName: generateTableName(),
+        players:[],
+        numberOfPlayer: 0,
+        dealer : null,
+        stack: stack
+    };
+
     
-}
-addMeToTable = function(player){
-    this.players.push(player);
+    return table;
 }
 
-hit = function(player){
-    
+exports.addMeToTable=function(player,table){
+
+    table.players.push(player);
+    table.numberOfPlayer=table.numberOfPlayer+1;
+    return table;
 }
 
-stand = function(player){
-    
+
+function getPlayer(playerId){
+
 }
 
-deal = function(player){
-    
+exports.findTableForMe=function(tables){
+
+    var temp = tables.filter(function(item){
+        return item.numberOfPlayer < 3;
+    });
+
+    if(temp.length===0){
+        temp = createNewTable();
+        tables.push(temp);
+        return temp;
+    }else{
+        return temp[0];
+    }
 }
+
+
